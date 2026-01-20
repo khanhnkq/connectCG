b0c3b1b0725d0ebea7c47082ee9985de5a6eeaea
HoangLeoO <leoo09012001@gmail.com>
2026-01-20 15:32:55 +0700

    code của hoang

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,34 @@ import UserProfile from './pages/dashboard/UserProfile';
 import Newsfeed from './pages/dashboard/Newsfeed';
 import FriendRequests from './pages/dashboard/FriendRequests';
 import MemberProfile from './pages/dashboard/MemberProfile';
+import AdminGroupsManager from "./pages/admin-website/AdminGroupsManager.jsx";
+import AdminMembersManager from "./pages/admin-website/AdminMembersManager.jsx";
+import MainFeedManager from "./pages/admin-website/MainFeedManager.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/registration/step-1" element={<Step1 />} />
-      <Route path="/registration/step-2" element={<Step2 />} />
-      <Route path="/dashboard/newsfeed-1" element={<NewsfeedDashboard1 />} />
-      <Route path="/dashboard/groups" element={<GroupsManagement />} />
-      <Route path="/dashboard/chat" element={<ChatInterface />} />
-      <Route path="/dashboard/profile/public" element={<UserProfile />} />
-      <Route path="/dashboard/feed" element={<Newsfeed />} />
-      <Route path="/dashboard/requests" element={<FriendRequests />} />
-      <Route path="/dashboard/profile/view" element={<MemberProfile />} />
-      <Route path="/search/members" element={<AdvancedMemberSearch />} />
-    </Routes>
+    <>
+      <ToastContainer position="top-right" autoClose={3000} theme="dark" />
+      <Routes>
+
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/registration/step-1" element={<Step1 />} />
+        <Route path="/registration/step-2" element={<Step2 />} />
+        <Route path="/dashboard/newsfeed-1" element={<NewsfeedDashboard1 />} />
+        <Route path="/dashboard/groups" element={<GroupsManagement />} />
+        <Route path="/dashboard/chat" element={<ChatInterface />} />
+        <Route path="/dashboard/profile/public" element={<UserProfile />} />
+        <Route path="/dashboard/feed" element={<Newsfeed />} />
+        <Route path="/dashboard/requests" element={<FriendRequests />} />
+        <Route path="/dashboard/profile/view" element={<MemberProfile />} />
+        <Route path="/search/members" element={<AdvancedMemberSearch />} />
+        <Route path="/admin-website/groups" element={<AdminGroupsManager />} />
+        <Route path="/admin-website/members" element={<AdminMembersManager />} />
+        <Route path="/admin-website/contents" element={<MainFeedManager />} />
+      </Routes>
+    </>
   );
 }
 
